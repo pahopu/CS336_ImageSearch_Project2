e1d73acfa293d0c85b4e1ff9a54adc5c530e55bc
pahopu <20520278@gm.uit.edu.vn>
2023-01-27 23:08:46 +0700

    Update indexing.py

--- systems/indexing.py
+++ systems/indexing.py
@@ -3,9 +3,10 @@ from image_search_system import ImageSearch_System as ISS
 if __name__ == '__main__':
     # Name of dataset
     dataset_name = 'oxbuild'
+    method = 'VGG16'
 
     # Create Image Search System object
-    IS = ISS(dataset_name)
+    IS = ISS(dataset_name, method)
 
     # Extract all features
     IS.indexing()
