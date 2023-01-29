7041c5110f15c80c78ae7d5f5e8e4ae929a3fe84
pahopu <20520278@gm.uit.edu.vn>
2023-01-29 15:45:17 +0700

    Update indexing.py

--- systems/indexing.py
+++ systems/indexing.py
@@ -3,7 +3,7 @@ from image_search_system import ImageSearch_System as ISS
 if __name__ == '__main__':
     # Name of dataset and method
     dataset_name = 'oxbuild'
-    method = 'VGG16'
+    method = 'Xception'
 
     # Create Image Search System object
     IS = ISS(dataset_name, method)
