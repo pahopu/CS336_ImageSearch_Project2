bc1b9d4f3fcfe80d7eca58e32608df6032606480
pahopu <20520278@gm.uit.edu.vn>
2023-01-31 00:06:07 +0700

    Add retrieve_image.py

---
+++ systems/retrieve_image.py
@@ -0,0 +1,13 @@
+from image_search_system import ImageSearch_System as ISS
+
+if __name__ == '__main__':
+    # Name of dataset and method
+    dataset_name = 'oxbuild'
+    method = 'Xception'
+
+    # Create Image Search System object
+    IS = ISS(dataset_name, method)
+
+    # Create image path, retrieve and print result
+    image_path = 'datasets/oxbuild/images/all_souls_000000.jpg'
+    IS.retrieve_image_and_print(image_path)
