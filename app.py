77e4d17b060dbb0fd2b016672e83126308d00ce0
pahopu <20520278@gm.uit.edu.vn>
2023-02-10 23:44:29 +0700

    Crop image

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import shutil
 import numpy as np
+import cv2
+import glob
 
-from click_and_drop import cropper
 from flask import Flask, render_template, request
 from systems.image_search_system import ImageSearch_System as ISS
 
@@ -15,7 +16,14 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html", CROP=False)
+    removing_files = glob.glob('static/*.jpg')
+    for i in removing_files:
+        try:
+            int(i[-5])
+            os.remove(i)
+        except:
+            continue
+    return render_template("upload.html")
 
 
 @app.route("/result", methods=["GET", "POST"])
@@ -27,6 +35,10 @@ def return_res():
     K = request.form.get("K")
     K = int(K) if K != "" else 3
     method = request.form.get("method")
+    x = int(request.form.get("x"))
+    y = int(request.form.get("y"))
+    width = int(request.form.get("img_width"))
+    height = int(request.form.get("img_height"))
 
     file.save(os.path.join(
         app.config['UPLOAD_FOLDER'],
@@ -34,15 +46,17 @@ def return_res():
     ))
 
     dataset_name = 'oxbuild'
-    crop = request.form.get("crop")
+    
+    img_query = cv2.imread("query.jpg")[y:y+height, x:x+width]
 
-    if crop:
-        cropper("query.jpg")
+    cv2.imwrite("query.jpg", img_query)
     shutil.copy("query.jpg", dst_dir + "query.jpg")
 
     IS = ISS(dataset_name, method)
     IS.indexing()
+
     res, time = IS.retrieve_image("query.jpg", K)
+
     paths = list(np.array(res)[:, 0])
     paths = [str(p) for p in paths]
 
