824e0dc8b0c9d3759eb9978aadcf58d232cd9f34
pahopu <20520278@gm.uit.edu.vn>
2023-02-11 13:49:40 +0700

    Update

--- systems/evaluating.py
+++ systems/evaluating.py
@@ -3,7 +3,7 @@ from image_search_system import ImageSearch_System as ISS
 if __name__ == '__main__':
     # Name of dataset and method
     dataset_name = 'oxbuild'
-    method = 'Xception'
+    method = 'VGG16'
 
     # Create Image Search System object
     IS = ISS(dataset_name, method)
