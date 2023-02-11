07d19f2cc50dff79f1a00c6449ddc33525c3df5e
pahopu <20520278@gm.uit.edu.vn>
2023-02-11 14:11:23 +0700

    Update

--- systems/indexing.py
+++ systems/indexing.py
@@ -2,8 +2,8 @@ from image_search_system import ImageSearch_System as ISS
 
 if __name__ == '__main__':
     # Name of dataset and method
-    dataset_name = 'paris'
-    method = 'InceptionV3'
+    dataset_name = 'oxbuild'
+    method = 'Xception'
 
     # Create Image Search System object
     IS = ISS(dataset_name, method)
