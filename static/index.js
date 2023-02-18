dba8e2440e43f530d82c476643216843b88566e2
hnvkhanh <hnvk101@gmail.com>
2023-02-19 00:07:48 +0700

    fix commit error

--- static/index.js
+++ static/index.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 var inputImage = document.getElementsByClassName('cropped-image')[0]
 
 inputImage.addEventListener("change", () => {
@@ -8,37 +6,3 @@ inputImage.addEventListener("change", () => {
         alert("hehehehheh")
     }
 })
-=======
-var dismissBtn = $('.dm-modal');
-
-
-var numImage = document.getElementById("range")
-numImage.innerHTML = document.getElementById("customRange").value
-document.getElementById("customRange").addEventListener("change", (e) => {
-	value = e.target.value
-	numImage.innerHTML = value
-})
-
-//for (let i = 0; i < dismissBtn.length; i++) {
-//    dismissBtn[i].addEventListener("click", () => {
-//        $("form")[0].reset()
-//    })
-//}
->>>>>>> 980a0eea797939db785415859ede40acbc451f7c
-=======
-var dismissBtn = $('.dm-modal');
-
-
-var numImage = document.getElementById("range")
-numImage.innerHTML = document.getElementById("customRange").value
-document.getElementById("customRange").addEventListener("change", (e) => {
-	value = e.target.value
-	numImage.innerHTML = value
-})
-
-//for (let i = 0; i < dismissBtn.length; i++) {
-//    dismissBtn[i].addEventListener("click", () => {
-//        $("form")[0].reset()
-//    })
-//}
->>>>>>> ff67239562e1965db44a89e3fedd424cc2c12461
